8a1891bf13168321bf83688cd4384b4c960345cf
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-13 03:33:47 +0530

    Updated for release 2.0.0

--- Package.swift
+++ Package.swift
@@ -15,16 +15,19 @@ let package = Package(
         .target(
             name: "FindInPageIOSJSSupport_Aggregation",
             dependencies: ["FindInPageIOSJSSupport"],
+            resources: [.process("jsSources")],
+            packageAccess: false,
             swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
         ),
         .binaryTarget(
             name: "FindInPageIOSJSSupport",
-            url: "https://github.com/swift-precompiled/ios-js-support/releases/download/2.0.0/FindInPageIOSJSSupport-c407dca72c7574dad3f5f1a5a8979e0450c6b7c1956e7183a62c7b95d37104f6.xcframework.zip",
-            checksum: "c407dca72c7574dad3f5f1a5a8979e0450c6b7c1956e7183a62c7b95d37104f6"
+            url: "https://github.com/swift-precompiled/ios-js-support/releases/download/2.0.0/FindInPageIOSJSSupport-b1979a331d7a5c15fb03228c9278e14bfdd25bee9f94a182f935d1e02d713158.xcframework.zip",
+            checksum: "b1979a331d7a5c15fb03228c9278e14bfdd25bee9f94a182f935d1e02d713158"
         ),
         .target(
             name: "FindInPageIOSJSSupport_PrecompiledProduct",
-            dependencies: ["FindInPageIOSJSSupport_Aggregation"]
+            dependencies: ["FindInPageIOSJSSupport_Aggregation"],
+            packageAccess: false
         )
     ]
 )
